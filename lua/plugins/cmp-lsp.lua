a42bef6ff4ea68b061512d9b2692602b03d56dc3
M0nenvoy <goldseeker68191@gmail.com>
2022-11-29 21:31:47 +0400

    Got rid of the deprecated functionality

--- lua/plugins/cmp-lsp.lua
+++ lua/plugins/cmp-lsp.lua
@@ -60,7 +60,7 @@ cmp.setup.filetype('TelescopePrompt', {
 
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lcfg = require('lspconfig')
 
 lcfg['emmet_ls'].setup  {
